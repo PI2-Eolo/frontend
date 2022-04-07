b86a7cada481e9a0eea7d0e286081e0ffe8806da
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-07 09:34:03 -0300

    Creation of basic component

---
+++ src/components/Selector/index.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+interface SelectorProps {
+
+}
+
+const Selector = (props:SelectorProps) => {
+    return (
+        <div >
+            Selector component
+        </div>
+    )
+};
+
+export default Selector;
