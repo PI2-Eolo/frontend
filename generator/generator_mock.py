adad66d4b848eeab8549e3f13fac2520af55ffbb
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-22 14:32:42 -0300

    Correction of values transiction

--- generator/generator_mock.py
+++ generator/generator_mock.py
@@ -7,37 +7,6 @@ port=1883
 MQTT_TOPIC = [('sensor/consumption', 0), ('sensor/en_prod', 0), ('sensor/humidity', 0), ('sensor/temperature', 0)]
 
 
-def generate_numbers(min, max, k=1):
-    result = []
-    seen = set()
-    for i in range(k):
-        x = random.uniform(min, max)
-        while x in seen:
-            x = random.uniform(min, max)
-        seen.add(x)
-        result.append(x)
-    return result
-
-def get_ranges(topic):
-    min, max = topic
-    return generate_numbers(min, max, 1)[0]
-
-
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-
-def send_msg(topic, value):
-    client.publish(topic, value);
-
-client = mqtt.Client("EOLO")
-print("connecting to broker")
-client.connect(broker)
-client.loop_start()
-print("Subscribing to topics:");
-for topic, _ in MQTT_TOPIC:
-    print('\t- ', topic);
-client.subscribe(MQTT_TOPIC);
-
 topic_ranges = {
     'sensor/humidity': (0.2, 1),
     'sensor/consumption': (0.2, 1),
@@ -45,15 +14,51 @@ topic_ranges = {
     'sensor/temperature': (23.0, 24.3)
 }
 
+topic_values = { # Initial values
+    'sensor/humidity': 0.5, 
+    'sensor/consumption': 0.5,
+    'sensor/en_prod': 0.5,
+    'sensor/temperature': 23.5
+}
+
+def generate_numbers(min_value, max_value, last_value):
+    l = 0 if last_value == min_value else -0.1
+    r = 0 if last_value == max_value else 0.1
+    x = random.uniform(l,r)
+    return last_value + x
+
+def get_ranges(topic):
+    min, max = topic_ranges[topic]
+    last_value = topic_values[topic]
+    return generate_numbers(min, max, last_value)
+
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+
+def send_msg(topic, value):
+    client.publish(topic, value)
+
+client = mqtt.Client("EOLO")
+print("connecting to broker")
+client.connect(broker)
+client.loop_start()
+print("Subscribing to topics:")
+for topic, _ in MQTT_TOPIC:
+    print('\t- ', topic)
+client.subscribe(MQTT_TOPIC)
+
+
 i = 0
 while True:
-    i+=1;
+    i+=1
 
     if i >= 4:
-        i = 0;
-        time.sleep(2);
+        i = 0
+        time.sleep(2)
 
-    val = get_ranges(topic_ranges[MQTT_TOPIC[i][0]])
+    val = get_ranges(MQTT_TOPIC[i][0])
+    print(MQTT_TOPIC[i][0], val)
     send_msg(MQTT_TOPIC[i][0], val)
 
 client.loop_stop()
