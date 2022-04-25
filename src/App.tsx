27ac85db808a6b8315cc02888cfca1d2e5d9f373
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-25 19:07:23 -0300

    Remove of unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -118,14 +118,6 @@ function App() {
     };
   }, []);
 
-  const data = [
-    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-    { name: "Page B", uv: 300, pv: 4567, amt: 2400 },
-    { name: "Page C", uv: 200, pv: 1398, amt: 2400 },
-    { name: "Page D", uv: 278, pv: 9800, amt: 2400 },
-    { name: "Page E", uv: 189, pv: 4800, amt: 2400 },
-  ];
-
   return (
     <div className="App">
       <Header />
